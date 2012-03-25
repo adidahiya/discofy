75f225ef200b6475306b311b54286ec8c789fa6b
Adi Dahiya <adidas006@gmail.com>
2012-03-24 23:21:33 -0400

    Worked on creating new game and playlist

--- scripts/discofy.js
+++ scripts/discofy.js
@@ -5,9 +5,9 @@ var sp = getSpotifyApi(1),
     auth = sp.require('sp://import/scripts/api/auth');
 
 /* Authentication params */
-var permissions = ['user_actions.music'],
+var permissions = [],
     app_id = '192337954211372',
-    request_url = 'https://graph.facebook.com/me/music.listens';
+    request_url = 'https://graph.facebook.com/me';
 
 
 exports.init = init;
@@ -15,14 +15,22 @@ exports.init = init;
 function init() {
     auth.authenticateWithFacebook(app_id, permissions, {
         onSuccess : function(accessToken, ttl) {
-            var url = request_url + '?access_token=' + accessToken;
-            $.getJSON(url, function(data) {
-                var listens = data.data;
-                for(var i=0;i<listens.length;i++) {
-                    var tracklink = listens[i].data.song.url;
-                    var trackname = listens[i].data.song.title;
-                    $('#listens').append('<li><a href="' + tracklink + '">' + trackname + '</a></li>');
+            var friends_url = request_url + '/friends?access_token=' + accessToken;
+            $.getJSON(friends_url, function(data) {
+                var friends = data.data,
+                    $select = $("select#players");
+                for (var i = 0; i < friends.length; i++) {
+                    var friend = friends[i];
+                    $select.append("<option value='" + friend.id + "'>" + friend.name + "</option>");
                 }
+                
+                $(".chzn-select").chosen();
+                $("#new-game-form").hide();
+            });
+            
+            var me_url = request_url + '?access_token=' + accessToken;
+            $.getJSON(me_url, function(data) {
+                $("input#author-id").val(data.id);
             });
         },
         onFailure : function(error) {
@@ -31,3 +39,31 @@ function init() {
         onComplete : function() { }
     });
 }
+
+$(document).ready(function() {
+    var $new_game_form = $("#new-game-form"),
+        tracks = models.library.tracks;
+    
+    // User ids of owners and participants, Spotify URI for new playlist
+    $("#new-game").on("click", function(event) {
+        $new_game_form.slideToggle("fast");
+    });
+    
+    $("button#create-game").on("click", function(event) {
+        var name = $("input#playlist-name").val(),
+            newPlaylist = new models.Playlist(name);
+        
+        newPlaylist.add(models.Track.fromURI(tracks[0].data.uri));
+        console.log(newPlaylist);
+        
+        /*
+        var player = new views.Player();
+        player.context = newPlaylist;
+        $('#player').append(player.node);
+        var playlist = new views.List(newPlaylist);
+        $('#player').append(playlist.node);
+        */
+        
+        $("input#playlist-uri").val(newPlaylist.uri);
+    });
+});
