018c982392ce5d6bd413dd5969da57fa6078eed6
Adi Dahiya <adidas006@gmail.com>
2012-03-25 01:28:41 -0400

    Added requests for data from our server

--- scripts/discofy.js
+++ scripts/discofy.js
@@ -2,7 +2,8 @@
 var sp = getSpotifyApi(1),
     models = sp.require('sp://import/scripts/api/models'),
     views = sp.require('sp://import/scripts/api/views'),
-    auth = sp.require('sp://import/scripts/api/auth');
+    auth = sp.require('sp://import/scripts/api/auth'),
+    friendsList = [];
 
 /* Authentication params */
 var permissions = [],
@@ -13,6 +14,8 @@ var permissions = [],
 exports.init = init;
 
 function init() {
+    var $new_game_form = $("#new-game-form");
+    
     auth.authenticateWithFacebook(app_id, permissions, {
         onSuccess : function(accessToken, ttl) {
             var friends_url = request_url + '/friends?access_token=' + accessToken;
@@ -22,15 +25,27 @@ function init() {
                 for (var i = 0; i < friends.length; i++) {
                     var friend = friends[i];
                     $select.append("<option value='" + friend.id + "'>" + friend.name + "</option>");
+                    friendsList.push(friend.id);
                 }
                 
                 $(".chzn-select").chosen();
-                $("#new-game-form").hide();
+                $new_game_form.hide();
+                $new_game_form.css("visibility", "visible");
             });
             
             var me_url = request_url + '?access_token=' + accessToken;
             $.getJSON(me_url, function(data) {
                 $("input#author-id").val(data.id);
+            });
+            
+            var listening_url = request_url + '/music.listens?access_token=' + accessToken;
+            $.getJSON(listening_url, function(data) {
+                var listens = data.data;
+                for(var i=0;i<listens.length;i++) {
+                    var tracklink = listens[i].data.song.url;
+                    var trackname = listens[i].data.song.title;
+                    $('#listens').append('<li><a href="' + tracklink + '">' + trackname + '</a></li>');
+                }
             });
         },
         onFailure : function(error) {
@@ -38,32 +53,60 @@ function init() {
         },
         onComplete : function() { }
     });
+    
+    var discofy_url = "";
+    $.get(discofy_url, {
+        user : $("input#author-id").val()
+    }, function(data) {        
+        for (var i = 0; i < data.length; i++) {
+            var name = data[i].name,
+                count = data[i].count;
+            $("ul#current-games").append("<li><a href='#!'>" + name + "</a> - " + count + " songs</li>");
+        }
+    });
 }
 
 $(document).ready(function() {
     var $new_game_form = $("#new-game-form"),
         tracks = models.library.tracks;
     
-    // User ids of owners and participants, Spotify URI for new playlist
     $("#new-game").on("click", function(event) {
         $new_game_form.slideToggle("fast");
     });
     
     $("button#create-game").on("click", function(event) {
         var name = $("input#playlist-name").val(),
-            newPlaylist = new models.Playlist(name);
+            author = $("input#author-id").val();
         
-        newPlaylist.add(models.Track.fromURI(tracks[0].data.uri));
-        console.log(newPlaylist);
-        
-        /*
-        var player = new views.Player();
-        player.context = newPlaylist;
-        $('#player').append(player.node);
-        var playlist = new views.List(newPlaylist);
-        $('#player').append(playlist.node);
-        */
-        
-        $("input#playlist-uri").val(newPlaylist.uri);
+        if (name !== "") {
+            /*
+            playlist = models.Playlist.fromURI(uri);
+            console.log(playlist);
+            
+            var player = new views.Player();
+            player.context = newPlaylist;
+            $('#player').append(player.node);
+            var playlist = new views.List(newPlaylist);
+            $('#player').append(playlist.node);
+            */
+            
+            var discofy_url = "",
+                selected = $("ul.chzn-results").find(".result-selected"),
+                players = [];
+            
+            // Get selected friend ids and push into players array for POST request
+            for (var i = 0; i < selected.length; i++) {
+                var index = selected[i].id.substr(15);
+                players.push(friendsList[parseInt(index)]);
+            }
+            
+            $.post(discofy_url, { 
+                name : name,
+                author : author,
+                players : players
+            }, function(data) {
+                console.log("Request callback");
+            });
+        }
     });
 });
